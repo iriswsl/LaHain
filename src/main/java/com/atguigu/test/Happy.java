0339dea3c0cd862b1b4afc7d42a46747aaecdefa
wsl <wsl@atguigu.com>
2019-10-12 19:37:21 +0800

    good

--- src/main/java/com/atguigu/test/Happy.java
+++ src/main/java/com/atguigu/test/Happy.java
@@ -3,6 +3,7 @@ package com.atguigu.test;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("print on right...perfiect");
+		System.out.println("print on hot_fix");
 	}
 }
  
